1c59cecf8dd656e0cca9178bd6d2c510a53573bd
weixiangnan <13811658342@163.com>
2020-07-14 10:17:57 +0800

    123

--- src/main/java/com/wxn/controller/TestController.java
+++ src/main/java/com/wxn/controller/TestController.java
@@ -2,16 +2,27 @@ package com.wxn.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Controller
 public class TestController {
 
     @RequestMapping("/test")
-    public String test(HttpServletRequest request) {
-        System.out.println("hello~");
-        request.setAttribute("name", "w");
+    public String test(HttpServletRequest request) throws InterruptedException {
         return "test";
+    }
+
+    @ResponseBody
+    @RequestMapping("/test1")
+    public String test1(HttpServletRequest request) throws InterruptedException {
+        while(true) {
+            if(LocalDateTime.now().getMinute() % 2 == 0) {
+                return ":";
+            }
+            Thread.sleep(500);
+        }
     }
 }
